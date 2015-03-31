02522fe410c405fea8738004a9144ee6c840f4d2
Gabriel Burt <gabriel.burt@gmail.com>
2015-03-30 20:02:56 -0500

    add #subscribed? to helpers called w/o namespace

--- lib/redis/namespace.rb
+++ lib/redis/namespace.rb
@@ -194,6 +194,7 @@ class Redis
       "echo"             => [],
       "ping"             => [],
       "disconnect!"      => [],
+      "subscribed?"      => [],
     }
     ADMINISTRATIVE_COMMANDS = {
       "bgrewriteaof"     => [],
