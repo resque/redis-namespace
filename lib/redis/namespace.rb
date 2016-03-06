d7c253a271b9d60a983aef4a2c78103ccc90abfd
Max Wallace <maxfield.wallace@gmail.com>
2016-03-05 22:32:12 -0500

    Add connected? to HELPER_COMMANDS

--- lib/redis/namespace.rb
+++ lib/redis/namespace.rb
@@ -194,6 +194,7 @@ class Redis
       "echo"             => [],
       "ping"             => [],
       "disconnect!"      => [],
+      "connected?"       => [],
     }
     ADMINISTRATIVE_COMMANDS = {
       "bgrewriteaof"     => [],
