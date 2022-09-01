2c1ae0f3c4b06b92712461dea387234a40a8164b
Jean byroot Boussier <jean.boussier+github@shopify.com>
2022-09-01 14:05:45 +0200

    Allow Redis#close (#209)
    It's been the recommended way to close the connection
    for a good decade, and it's the method ued by connection_pool
    gem.
    
    Co-authored-by: Jean Boussier <jean.boussier@gmail.com>

--- lib/redis/namespace.rb
+++ lib/redis/namespace.rb
@@ -202,6 +202,7 @@ class Redis
     HELPER_COMMANDS = {
       "auth"             => [],
       "disconnect!"      => [],
+      "close"            => [],
       "echo"             => [],
       "ping"             => [],
       "time"             => [],
